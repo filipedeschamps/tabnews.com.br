6e27bfd63e58183c92892102aafde5f28665b1f0
Filipe Deschamps <filiped@gmail.com>
2021-05-05 15:54:42 -0700

    bootstrap: add first dependencies

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../css/tailwind.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="grid grid-cols-2">
+      <div>Lista de conteúdos</div>
+      <div>Conteúdo</div>
+    </div>
+  );
+}
