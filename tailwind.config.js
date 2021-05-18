98de45a7949bfdad544f98aecb714ebb18d50fa6
Filipe Deschamps <filiped@gmail.com>
2021-05-18 11:26:08 -0700

    layout: add layout-08

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        warmGray: colors.warmGray,
+      },
+    },
   },
   variants: {
     extend: {},
