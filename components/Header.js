300d0ab10941b9ad9cc2dab7096ba681ffb49265
Pedro Cruz <pedro.scruzzz@gmail.com>
2021-05-31 00:22:13 -0300

    chore: cleaning unnecessary imports

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,6 @@
-import Link from "next/link";
 import { CgTab } from "react-icons/cg";
-import { IoChatbox } from "react-icons/io5";
-import { BsArrowReturnRight } from "react-icons/bs";
-import { FaStar } from "react-icons/fa";
-import { VscCircleFilled } from "react-icons/vsc";
-import { MdStars } from "react-icons/md";
 import { MdAccountCircle } from "react-icons/md";
 import { MdMonetizationOn } from "react-icons/md";
-import { RiMoneyDollarCircleFill } from "react-icons/ri";
-
-import { username } from "react-lorem-ipsum";
-import { LoremIpsum } from "lorem-ipsum";
 import { ToggleButton} from './ToggleButton'
 
 export function Header() {
--- components/ToggleButton.js
+++ components/ToggleButton.js
@@ -1,17 +1,3 @@
-import Link from "next/link";
-import { CgTab } from "react-icons/cg";
-import { IoChatbox } from "react-icons/io5";
-import { BsArrowReturnRight } from "react-icons/bs";
-import { FaStar } from "react-icons/fa";
-import { VscCircleFilled } from "react-icons/vsc";
-import { MdStars } from "react-icons/md";
-import { MdAccountCircle } from "react-icons/md";
-import { MdMonetizationOn } from "react-icons/md";
-import { RiMoneyDollarCircleFill } from "react-icons/ri";
-
-import { username } from "react-lorem-ipsum";
-import { LoremIpsum } from "lorem-ipsum";
-import { useEffect, useState, useCallback } from "react";
 import {useTheme} from '../hooks/themeModeProvider'
 
 export function ToggleButton() {
