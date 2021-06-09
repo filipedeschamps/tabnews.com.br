a29a83e6a24cb0cb3ceab533b9fb0e6af5e1dafd
Luan <ltoningalvan@gmail.com>
2021-06-09 15:16:01 -0300

    remove alterações feitas não necessárias

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
