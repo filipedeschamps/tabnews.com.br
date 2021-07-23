b7c4e3498f33d97d9f6cbda3d1c9937277ae18de
Filipe Deschamps <filiped@gmail.com>
2021-07-23 14:14:44 -0700

    fix: add `ssl` option when creating a connection pool
    But remove it if it's localhost, development or CI

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,26 @@
 import { Pool } from "pg";
 
 export default function DatabaseFactory() {
-  const pool = new Pool({
+  const poolConfiguration = {
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
-  });
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+
+  // https://github.com/filipedeschamps/tabnews.com.br/issues/84
+  if (
+    ["test", "development"].includes(process.env.NODE_ENV) ||
+    process.env.CI
+  ) {
+    delete poolConfiguration.ssl;
+  }
+
+  const pool = new Pool(poolConfiguration);
 
   async function query(query) {
     const results = await pool.query(query);
