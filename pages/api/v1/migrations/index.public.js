ca254c5498de61c11fc51c470f8f037fb311b398
Filipe Deschamps <filiped@gmail.com>
2021-09-06 19:55:02 -0700

    refactor: use `next-connect` to refactor `/api/v1/migrations`

--- pages/api/v1/migrations/index.public.js
+++ pages/api/v1/migrations/index.public.js
@@ -1,29 +1,53 @@
+import nextConnect from 'next-connect';
 import migratorFactory from 'infra/migrator.js';
 
-export default async function Migrations(request, response) {
-  const migrator = migratorFactory();
+const migrator = migratorFactory();
 
-  try {
-    if (request.method === 'GET') {
-      const pendingMigrations = await migrator.listPendingMigrations();
-      return response.status(200).json(pendingMigrations);
-    }
+export default nextConnect({
+  attachParams: true,
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+})
+  .use(authenticationHandler)
+  .use(authorizationHandler)
+  .get(getHandler)
+  .post(postHandler);
 
-    if (request.method === 'POST') {
-      const migratedMigrations = await migrator.runPendingMigrations();
+async function authenticationHandler(request, response, next) {
+  // TODO: implement authentication
+  console.log('Trying to authenticate');
+  next();
+}
 
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
+async function authorizationHandler(request, response, next) {
+  // TODO: implement authorization
+  console.log('Trying to authorize');
+  next();
+}
 
-      return response.status(200).json(migratedMigrations);
-    }
+async function getHandler(request, response) {
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pendingMigrations);
+}
 
-    // TODO: create a pattern with Custom Errors.
-    // Do not rely on this response right now.
-    return response.status(404).json({ error: 'Not Found' });
-  } catch (error) {
-    console.log(error);
-    return response.status(500).json({ error: error.message });
+async function postHandler(request, response) {
+  const migratedMigrations = await migrator.runPendingMigrations();
+
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
+
+  return response.status(200).json(migratedMigrations);
+}
+
+// TODO: create a pattern with Custom Errors.
+// Do not rely on this responses right now.
+
+async function onNoMatchHandler(request, response) {
+  return response.status(404).json({ error: 'Not Found' });
+}
+
+function onErrorHandler(error, req, res, next) {
+  console.log(error);
+  res.status(500).json({ error: error.message });
 }
