8318d940bd656fdc7d0b834d64ea0487057676f4
rodrigoKulb <kulb.rodrigo@gmail.com>
2021-09-07 14:43:33 -0300

    feat(migrationuser): suggestion of user table migration
    Conforme issues CRUD de Usuários para criar a tabela usuario montei essa PR para iniciarmos a
    estrutura.

---
+++ infra/migrations/1631027616537_CreateUser.js
@@ -0,0 +1,17 @@
+/* creating the user table */
+
+exports.up = (pgm) => {
+    pgm.createTable('users', {
+        id: 'serial',
+        name: { type: 'string', length: 128, notNull: true },
+        email: { type: 'string', length: 255, notNull: true, unique: true },
+        password: { type: 'string', length: 255, notNull: true },
+        avatar_file: { type: 'string', length: 255, notNull: true },
+        created_at: { type: 'timestamp', default: pgm.func("(now() at time zone 'utc')") },
+        updated_at: { type: 'timestamp', default: pgm.func("(now() at time zone 'utc')") }
+    })
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('users');
+};
