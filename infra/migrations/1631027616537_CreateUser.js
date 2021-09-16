22b20f8420cf94cba6d74d9d4e2a3d15eb0d8f2d
Rodrigo Kulb <rodrigo.kulb@varapido.com.br>
2021-09-16 11:21:19 -0300

    Update 1631027616537_CreateUser.js
    Removendo o notNull para o avatar_file

--- infra/migrations/1631027616537_CreateUser.js
+++ infra/migrations/1631027616537_CreateUser.js
@@ -6,7 +6,7 @@ exports.up = (pgm) => {
         name: { type: 'string', length: 128, notNull: true },
         email: { type: 'string', length: 255, notNull: true, unique: true },
         password: { type: 'string', length: 255, notNull: true },
-        avatar_file: { type: 'string', length: 255, notNull: true },
+        avatar_file: { type: 'string', length: 255 },
         created_at: { type: 'timestamp', default: pgm.func("(now() at time zone 'utc')") },
         updated_at: { type: 'timestamp', default: pgm.func("(now() at time zone 'utc')") }
     })
