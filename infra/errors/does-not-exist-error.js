51aab148fa105ed6e786fa8186f5927cbfa95ddb
liverday <vitor.medeiro10@gmail.com>
2021-09-24 12:10:10 -0300

    refactor: do lint on projet to avoid prettier warning

--- infra/errors/does-not-exist-error.js
+++ infra/errors/does-not-exist-error.js
@@ -1,7 +1,7 @@
-import BaseError from "./base-error";
+import BaseError from './base-error';
 
 export default class DoesNotExistError extends BaseError {
   constructor({ message, action, type }) {
-    super({ code: 404, message, action, type })
+    super({ code: 404, message, action, type });
   }
 }
