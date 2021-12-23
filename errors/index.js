487ba237594b8e751f04cab57315314cc8148aa3
Filipe Deschamps <filiped@gmail.com>
2021-12-23 13:14:34 -0300

    feat(errors): add DatabaseError and ValidationError
    Also, make some errors receive other properties to sync them up.

--- errors/index.js
+++ errors/index.js
@@ -1,38 +1,59 @@
 import { v4 as uuid } from 'uuid';
 
 class BaseError extends Error {
-  constructor({ message, stack, action, statusCode, requestId, errors }) {
+  constructor({ message, stack, action, statusCode, errorId, requestId, errors }) {
     super();
     this.name = this.constructor.name;
-    this.stack = stack;
     this.message = message;
     this.action = action;
     this.statusCode = statusCode;
-    this.errors = errors;
-    this.errorId = uuid();
+    this.errorId = errorId || uuid();
     this.requestId = requestId;
+    this.stack = stack;
   }
 }
 
 export class InternalServerError extends BaseError {
-  constructor({ requestId, stack }) {
+  constructor({ requestId, errorId, stack }) {
     super({
       message: 'Um erro interno não esperado aconteceu.',
       action: "Informe ao suporte o valor encontrado nos campos 'errorId'.",
       statusCode: 500,
       requestId: requestId,
+      errorId: errorId,
       stack: stack,
     });
   }
 }
 
 export class NotFoundError extends BaseError {
-  constructor({ requestId }) {
+  constructor({ requestId, errorId, stack }) {
     super({
       message: 'Não foi possível encontrar este recurso no sistema.',
       action: 'Verifique se o caminho (PATH) e o método (GET, POST, PUT, DELETE) estão corretos.',
       statusCode: 404,
       requestId: requestId,
+      errorId: errorId,
+      stack: stack,
+    });
+  }
+}
+export class DatabaseError extends BaseError {
+  constructor({ message, stack }) {
+    super({
+      message: message,
+      action: 'Verifique se os dados enviados ao banco respeitam os tipos e tamanhos das colunas.',
+      stack: stack,
+    });
+  }
+}
+export class ValidationError extends BaseError {
+  constructor({ message, stack }) {
+    super({
+      message: message,
+      action: 'Ajuste os dados enviados e tente novamente.',
+      statusCode: 400,
+      stack: stack,
     });
   }
 }
