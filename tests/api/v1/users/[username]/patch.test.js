8bdf0a99fcd855a863a5b2c5e56b26fa06d742a4
Filipe Deschamps <filiped@gmail.com>
2022-02-10 13:57:08 -0800

    test(user): tests anonymous user trying to patch other user

--- tests/api/v1/users/[username]/patch.test.js
+++ tests/api/v1/users/[username]/patch.test.js
@@ -12,6 +12,38 @@ beforeAll(async () => {
 });
 
 describe('PATCH /api/v1/users/[username]', () => {
+  //TODO: test with expired session
+
+  describe('Anonymous user', () => {
+    test('Patching other user', async () => {
+      let secondUser = await orchestrator.createUser();
+
+      const response = await fetch(`${orchestrator.webserverUrl}/api/v1/users/${secondUser.username}`, {
+        method: 'patch',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+          username: 'regularUserPatchingOtherUser',
+        }),
+      });
+
+      const responseBody = await response.json();
+
+      expect(response.status).toEqual(403);
+      expect(responseBody.name).toEqual('ForbiddenError');
+      expect(responseBody.message).toEqual('Usuário não pode executar esta operação.');
+      expect(responseBody.action).toEqual('Verifique se este usuário possui a feature "update:user".');
+      expect(responseBody.statusCode).toEqual(403);
+      expect(uuidVersion(responseBody.errorId)).toEqual(4);
+      expect(uuidValidate(responseBody.errorId)).toEqual(true);
+      expect(uuidVersion(responseBody.requestId)).toEqual(4);
+      expect(uuidValidate(responseBody.requestId)).toEqual(true);
+      expect(responseBody.errorUniqueCode).toEqual('MODEL:AUTHORIZATION:CAN_REQUEST:FEATURE_NOT_FOUND');
+    });
+  });
+
   describe('User with default features', () => {
     let firstUser;
     let firstUserSession;
