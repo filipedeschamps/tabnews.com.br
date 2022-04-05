592d54c22c0af9c32a1243db382c25989b105f7e
Filipe Deschamps <filiped@gmail.com>
2022-04-05 08:44:48 -0700

    chore(scripts): remove `download-collaborators-avatar`

--- scripts/download-collaborators-avatar/src/generate-collaborators-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-export default function generateCollboratorsJson(collaborators) {
-  const collaboratorsLogin = collaborators.map((collaborator) => {
-    return collaborator.login;
-  });
-
-  fs.writeFileSync('./src/contents/collaborators.json', JSON.stringify(collaboratorsLogin));
-}
--- scripts/download-collaborators-avatar/src/utils/fileExist.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-export default function fileExist(path) {
-  return path ? fs.existsSync(path) : false;
-}
