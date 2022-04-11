0650f89282a656a5c2a804c38d2604e5f1188c8e
Wandinho Oliveira <inovaprog@gmail.com>
2022-04-10 22:16:09 -0300

    feat(contents): [Draft] creates migrations for content entity
    style(lint): run lint fix
    
    revert(package.json): remove script to revert migration

---
+++ infra/migrations/1649621949432_create-content-table.js
@@ -0,0 +1,67 @@
+exports.up = async (pgm) => {
+  await pgm.createTable('contents', {
+    id: {
+      type: 'uuid',
+      default: pgm.func('gen_random_uuid()'),
+      notNull: true,
+      primaryKey: true,
+    },
+
+    owner_id: {
+      type: 'uuid',
+      notNull: true,
+    },
+
+    parent_id: {
+      type: 'uuid',
+      notNull: false,
+    },
+
+    slug: {
+      type: 'varchar(60)',
+    },
+
+    title: {
+      type: 'varchar(60)',
+    },
+
+    body: {
+      type: 'text',
+      notNull: true,
+    },
+
+    status: {
+      type: 'varchar(20)',
+      default: 'draft',
+      notNull: true,
+    },
+
+    source_url: {
+      type: 'varchar(100)',
+      notNull: false,
+    },
+
+    created_at: {
+      type: 'timestamp with time zone',
+      notNull: true,
+      default: pgm.func("(now() at time zone 'utc')"),
+    },
+
+    updated_at: {
+      type: 'timestamp with time zone',
+      notNull: true,
+      default: pgm.func("(now() at time zone 'utc')"),
+    },
+  });
+
+  await pgm.addConstraint(
+    'contents',
+    'slug_and_title_is_not_null_if_parent_id_is_null',
+    'CHECK (parent_id IS NOT NULL OR (slug IS NOT NULL AND title IS NOT NULL))'
+  );
+};
+
+exports.down = async (pgm) => {
+  await pgm.dropConstraint('contents', 'slug_and_title_is_not_null_if_parent_id_is_null');
+  await pgm.dropTable('contents');
+};
