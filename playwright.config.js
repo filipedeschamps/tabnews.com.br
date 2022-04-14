43de1128d454dde99ab0de2d8560f57d89e01fde
Andre F.Dias <andrefernandesdias@gmail.com>
2022-04-14 15:19:01 -0700

    test: updating playwright config to include chrome mobile

--- playwright.config.js
+++ playwright.config.js
@@ -63,12 +63,12 @@ const config = {
     //   },
     // },
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
     // {
     //   name: 'Mobile Safari',
     //   use: {
