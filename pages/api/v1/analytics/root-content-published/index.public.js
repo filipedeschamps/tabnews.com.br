745c808b1e92c1014bae82b2beb392cbed35e459
Filipe Deschamps <filiped@gmail.com>
2022-05-03 20:05:51 -0700

    feat: add `/status` page and temporary `/analytics` api endpoints

---
+++ pages/api/v1/analytics/root-content-published/index.public.js
@@ -0,0 +1,43 @@
+import nextConnect from 'next-connect';
+import controller from 'models/controller.js';
+import database from 'infra/database.js';
+
+export default nextConnect({
+  attachParams: true,
+  onNoMatch: controller.onNoMatchHandler,
+  onError: controller.onErrorHandler,
+})
+  .use(controller.injectRequestId)
+  .use(controller.logRequest)
+  .get(getHandler);
+
+async function getHandler(request, response) {
+  const results = await database.query(`
+  WITH range_values AS (
+    SELECT date_trunc('day', min(published_at)) as minval,
+           date_trunc('day', max(published_at)) as maxval
+    FROM contents),
+
+  day_range AS (
+    SELECT generate_series(minval, maxval, '1 day'::interval) as date
+    FROM range_values
+  ),
+
+  daily_counts AS (
+    SELECT date_trunc('day', created_at) as date,
+           count(*) as ct
+    FROM contents WHERE parent_id is null
+    GROUP BY 1
+  )
+
+  SELECT TO_CHAR(day_range.date :: DATE, 'dd/mm') as date,
+         daily_counts.ct as conteudos
+  FROM day_range
+  LEFT OUTER JOIN daily_counts on day_range.date = daily_counts.date;
+  `);
+
+  const usersCreated = results.rows;
+
+  response.setHeader('Cache-Control', 'public, s-maxage=1, stale-while-revalidate');
+  return response.status(200).json(usersCreated);
+}
---
+++ pages/status/index.public.js
@@ -0,0 +1,167 @@
+import useSWR from 'swr';
+import { Box, Heading, Label, LabelGroup } from '@primer/react';
+import { ResponsiveContainer, BarChart, Bar, Tooltip, XAxis } from 'recharts';
+
+import { DefaultLayout } from 'pages/interface/index.js';
+
+export default function Page() {
+  const { data: statusObject, isLoading: statusObjectIsLoading } = useSWR('/api/v1/status', { refreshInterval: 1000 });
+  const { data: usersCreated } = useSWR('/api/v1/analytics/users-created', { refreshInterval: 1000 });
+  const { data: rootContentPublished } = useSWR('/api/v1/analytics/root-content-published', { refreshInterval: 1000 });
+  const { data: childContentPublished } = useSWR('/api/v1/analytics/child-content-published', {
+    refreshInterval: 1000,
+  });
+
+  return (
+    <DefaultLayout metadata={{ title: 'Estatísticas e Status do Site' }}>
+      <Box sx={{ display: 'grid', width: '100%' }}>
+        <Heading as="h1">Estatísticas e Status do Site</Heading>
+
+        <Box>
+          <h2>Novos cadastros</h2>
+
+          <ResponsiveContainer width="100%" aspect={9}>
+            <BarChart height={400} data={usersCreated}>
+              <XAxis dataKey="date" tick={{ fontSize: 10 }} />
+              <Tooltip />
+              <Bar type="monotone" dataKey="cadastros" fill="#2da44e" />
+            </BarChart>
+          </ResponsiveContainer>
+        </Box>
+
+        <Box>
+          <h2>Novas publicações</h2>
+          <ResponsiveContainer width="100%" aspect={9}>
+            <BarChart height={400} data={rootContentPublished}>
+              <XAxis dataKey="date" tick={{ fontSize: 10 }} />
+              <Tooltip />
+              <Bar type="monotone" dataKey="conteudos" fill="#2da44e" />
+            </BarChart>
+          </ResponsiveContainer>
+        </Box>
+
+        <Box>
+          <h2>Novas respostas</h2>
+          <ResponsiveContainer width="100%" aspect={9}>
+            <BarChart height={400} data={childContentPublished}>
+              <XAxis dataKey="date" tick={{ fontSize: 10 }} />
+              <Tooltip />
+              <Bar type="monotone" dataKey="conteudos" fill="#2da44e" />
+            </BarChart>
+          </ResponsiveContainer>
+        </Box>
+
+        <Box>
+          <h2>Banco de Dados</h2>
+
+          {!statusObjectIsLoading && (
+            <Box sx={{ display: 'grid' }}>
+              <Box>
+                Status:{' '}
+                <Label variant={statusObject?.dependencies.database.status ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.database.status}
+                </Label>
+              </Box>
+              <Box>
+                Conexões disponíveis:{' '}
+                <Label variant={statusObject?.dependencies.database.max_connections > 70 ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.database.max_connections}
+                </Label>
+              </Box>
+              <Box>
+                Conexões abertas:{' '}
+                <Label variant={statusObject?.dependencies.database.opened_connections < 40 ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.database.opened_connections}
+                </Label>
+              </Box>
+              <Box>
+                Latência:{' '}
+                <LabelGroup>
+                  <Label variant={statusObject?.dependencies.database.latency.first_query < 200 ? 'success' : 'danger'}>
+                    {`${Math.round(statusObject?.dependencies.database.latency.first_query)}ms`}
+                  </Label>
+                  <Label
+                    variant={statusObject?.dependencies.database.latency.second_query < 200 ? 'success' : 'danger'}>
+                    {`${Math.round(statusObject?.dependencies.database.latency.second_query)}ms`}
+                  </Label>
+                  <Label variant={statusObject?.dependencies.database.latency.third_query < 200 ? 'success' : 'danger'}>
+                    {`${Math.round(statusObject?.dependencies.database.latency.third_query)}ms`}
+                  </Label>
+                </LabelGroup>
+              </Box>
+              <Box>
+                Versão do PostgreSQL:{' '}
+                <Label variant={statusObject?.dependencies.database.version ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.database.version}
+                </Label>
+              </Box>
+            </Box>
+          )}
+        </Box>
+
+        <Box>
+          <h2>Servidor Web</h2>
+
+          {!statusObjectIsLoading && (
+            <Box sx={{ display: 'grid' }}>
+              <Box>
+                Status:{' '}
+                <Label variant={statusObject?.dependencies.webserver.status ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.status}
+                </Label>
+              </Box>
+              <Box>
+                Provedor:{' '}
+                <Label variant={statusObject?.dependencies.webserver.provider ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.provider}
+                </Label>
+              </Box>
+              <Box>
+                Ambiente:{' '}
+                <Label variant={statusObject?.dependencies.webserver.environment ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.environment}
+                </Label>
+              </Box>
+              <Box>
+                Região na AWS:{' '}
+                <Label variant={statusObject?.dependencies.webserver.aws_region ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.aws_region}
+                </Label>
+              </Box>
+              <Box>
+                Região na Vercel:{' '}
+                <Label variant={statusObject?.dependencies.webserver.vercel_region ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.vercel_region}
+                </Label>
+              </Box>
+              <Box>
+                Timezone:{' '}
+                <Label variant={statusObject?.dependencies.webserver.timezone ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.timezone}
+                </Label>
+              </Box>
+              <Box>
+                Autor do último commit:{' '}
+                <Label variant={statusObject?.dependencies.webserver.last_commit_author ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.last_commit_author}
+                </Label>
+              </Box>
+              <Box>
+                Mensagem no commit:{' '}
+                <Label variant={statusObject?.dependencies.webserver.last_commit_message ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.last_commit_message}
+                </Label>
+              </Box>
+              <Box>
+                SHA do commit:{' '}
+                <Label variant={statusObject?.dependencies.webserver.last_commit_message_sha ? 'success' : 'danger'}>
+                  {statusObject?.dependencies.webserver.last_commit_message_sha}
+                </Label>
+              </Box>
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </DefaultLayout>
+  );
+}
