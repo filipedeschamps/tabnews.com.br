2ccec04c82b614f519c455a8b6de5d7bbe34206b
gabrieldev525 <gvictor525.gv@gmail.com>
2022-05-19 11:34:22 -0300

    fix(migrations): script de health check do postgres antes de executar as migrations

---
+++ infra/scripts/wait-for-db-connection-ready.js
@@ -0,0 +1,37 @@
+const { exec } = require('node:child_process')
+const { sleep } = require('../utils')
+
+
+/**
+ * Check Postgres database connection status
+ * This is a recursive function that'll execute health check pg_isready command until postgres is accepting connection
+ *
+ * References: https://www.postgresql.org/docs/current/app-pg-isready.html
+ *
+ * @param {number} tries_count - how many times the health check was executed
+ */
+const healthCheckDB = async (tries_count = 0) => {
+  const retryTimeMs = 3000  // 3 seconds
+  const retryTimeInSeconds = retryTimeMs / 1000
+
+  exec(
+    `docker exec postgres-dev pg_isready`,
+    async (error, stdout, stderr) => {
+      healthCheckStatus = stdout
+      console.log("health check postgres: ", healthCheckStatus)
+
+      if(healthCheckStatus?.indexOf('accepting connections') != -1)
+        return
+
+      console.log('Falha ao tentar conectar com o banco de dados')
+      console.log(`Uma nova tentativa será feita em ${retryTimeInSeconds.toFixed(2)}s ...`)
+      await sleep(retryTimeMs)
+
+      return healthCheckDB(healthCheckStatus, ++tries_count)
+    }
+  )
+}
+
+(async () => {
+  await healthCheckDB()
+})()
---
+++ infra/utils.js
@@ -0,0 +1,7 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = {
+  sleep
+}
