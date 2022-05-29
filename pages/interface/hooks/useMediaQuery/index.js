8dc7ff8bced1ccc31d3727073ee597ea4cf7b8cf
rodrigokulb <rodrigo.kulb@varapido.com.br>
2022-05-29 19:46:45 -0300

    fix(favicon): detect browser mode and use a different favicon
    Detect if the browser is using dark mode and use a different favicon

---
+++ pages/interface/hooks/useMediaQuery/index.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+}
