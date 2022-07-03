f698375c04e528801c18ec39a880202d377f55f5
isaqueveras <isaque.verasccb@gmail.com>
2022-07-03 17:20:54 -0300

    feat(ContentList): customizando o ContentList da página inicial #472

--- pages/interface/components/ContentList/index.js
+++ pages/interface/components/ContentList/index.js
@@ -1,5 +1,5 @@
 import useSWR from 'swr';
-import { Box, Link, Text } from '@primer/react';
+import { Box, Link, Text, Truncate } from '@primer/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@primer/octicons-react';
 
 import PublishedSince from 'pages/interface/components/PublishedSince';
@@ -15,45 +15,48 @@ export default function ContentList({ contentList, pagination, paginationBasePat
   return (
     <>
       <Box
+        width={'100%'}
         sx={{
-          display: 'table',
-          borderSpacing: '0 0.5rem',
+          display: 'flex',
+          flexDirection: 'column',
         }}>
         {list.length > 0 ? <RenderItems /> : <RenderEmptyMessage />}
       </Box>
 
-      <Box
-        sx={{
-          display: 'flex',
-          width: '100%',
-          justifyContent: 'center',
-          gap: 4,
-          m: 4,
-        }}>
-        {pagination.previousPage ? (
-          <Link href={previousPageUrl}>
-            <ChevronLeftIcon size={16} />
-            Anterior
-          </Link>
-        ) : (
-          <Text color="fg.muted">
-            <ChevronLeftIcon size={16} />
-            Anterior
-          </Text>
-        )}
+      {list.length > 0 && (
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'center',
+            gap: 4,
+            m: 4,
+          }}>
+          {pagination.previousPage ? (
+            <Link href={previousPageUrl}>
+              <ChevronLeftIcon size={16} />
+              Anterior
+            </Link>
+          ) : (
+            <Text color="fg.muted">
+              <ChevronLeftIcon size={16} />
+              Anterior
+            </Text>
+          )}
 
-        {pagination.nextPage ? (
-          <Link href={nextPageUrl}>
-            Próximo
-            <ChevronRightIcon size={16} />
-          </Link>
-        ) : (
-          <Text color="fg.muted">
-            Próximo
-            <ChevronRightIcon size={16} />
-          </Text>
-        )}
-      </Box>
+          {pagination.nextPage ? (
+            <Link href={nextPageUrl}>
+              Próximo
+              <ChevronRightIcon size={16} />
+            </Link>
+          ) : (
+            <Text color="fg.muted">
+              Próximo
+              <ChevronRightIcon size={16} />
+            </Text>
+          )}
+        </Box>
+      )}
     </>
   );
 
@@ -69,19 +72,42 @@ export default function ContentList({ contentList, pagination, paginationBasePat
     return list.map((contentObject, index) => {
       const itemCount = index + 1 + listNumberOffset;
       return (
-        <Box as="article" key={contentObject.id} sx={{ display: 'table-row' }}>
-          <Box sx={{ display: 'table-cell', pr: 2, textAlign: 'right' }}>
-            <Text sx={{ fontSize: 2, color: 'fg.default', fontWeight: 'semibold', textAlign: 'right' }}>
-              {itemCount}.
-            </Text>
+        <Box
+          mb={2}
+          maxWidth={'100vw'}
+          borderRadius={5}
+          as="article"
+          key={contentObject.id}
+          borderColor="border.default"
+          borderWidth={1}
+          borderStyle="solid"
+          display={'flex'}
+          flexDirection={'row'}>
+          <Box
+            p={3}
+            borderRight={'1px #d0d7de solid'}
+            maxWidth={'6vw'}
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}>
+            <Text sx={{ fontSize: 2, color: 'fg.subtle', fontWeight: 'semibold' }}>{itemCount}</Text>
           </Box>
-          <Box sx={{ display: 'table-cell', lineHeight: '20px' }}>
+          <Box
+            p={3}
+            width={'100%'}
+            maxWidth={'85vw'}
+            display={'flex'}
+            flexDirection={'column'}
+            justifyContent={'center'}>
             <Box>
-              <Link
-                sx={{ fontSize: 2, color: 'fg.default', fontWeight: 'semibold' }}
-                href={`/${contentObject.username}/${contentObject.slug}`}>
-                {contentObject.title}
-              </Link>
+              <Truncate as="text" inline maxWidth={'85%'}>
+                <Link
+                  muted
+                  sx={{ fontSize: 2, color: 'fg.default', fontWeight: 'bold' }}
+                  href={`/${contentObject.username}/${contentObject.slug}`}>
+                  {contentObject.title}
+                </Link>
+              </Truncate>
             </Box>
             <Box sx={{ fontSize: 0, color: 'neutral.emphasis' }}>
               <Text>
