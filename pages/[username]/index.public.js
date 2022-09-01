6e0f65b426e13b923194542f5caa606d97bac194
Filipe Deschamps <filiped@gmail.com>
2022-09-01 11:12:13 -0700

    feat(user): add heading with `username` to `/[username]` page and an options menu

--- pages/[username]/index.public.js
+++ pages/[username]/index.public.js
@@ -1,3 +1,5 @@
+import useSWR from 'swr';
+import { useState } from 'react';
 import { DefaultLayout, ContentList, useUser } from 'pages/interface/index.js';
 import user from 'models/user.js';
 import content from 'models/content.js';
@@ -6,25 +8,127 @@ import validator from 'models/validator.js';
 import { NotFoundError } from 'errors/index.js';
 import { FaUser } from 'react-icons/fa';
 import { useRouter } from 'next/router';
+import { Box, Pagehead, ActionMenu, ActionList, Flash, IconButton, useConfirm, LabelGroup, Label } from '@primer/react';
+import { KebabHorizontalIcon, TrashIcon } from '@primer/octicons-react';
 
-export default function Home({ contentListFound, pagination, username }) {
+export default function Home({ contentListFound, pagination, userFound: userFoundFallback }) {
+  const { data: userFound, mutate: userFoundMutate } = useSWR(`/api/v1/users/${userFoundFallback.username}`, {
+    fallbackData: userFoundFallback,
+    revalidateOnMount: true,
+  });
+
   const { user, isLoading } = useUser();
   const { push } = useRouter();
+  const confirm = useConfirm();
+  const [globalErrorMessage, setGlobalErrorMessage] = useState(false);
 
-  const isAuthenticatedUser = user && user.username === username;
+  const isAuthenticatedUser = user && user.username === userFound.username;
+
+  async function handleClickNuke() {
+    setGlobalErrorMessage(null);
+
+    const confirmDelete1 = await confirm({
+      title: `Atenção: Você está realizando um Nuke!`,
+      content: `Deseja banir o usuário "${userFound.username}" e desfazer todas as suas ações?`,
+    });
+
+    if (!confirmDelete1) return;
+
+    // Fake delay to avoid multiple accidental clicks
+    await new Promise((r) => setTimeout(r, 1000));
+
+    const confirmDelete2 = await confirm({
+      title: `Nuke em "${userFound.username}"`,
+      content: `Confirme novamente esta operação.`,
+    });
+
+    if (!confirmDelete2) return;
+
+    const payload = {
+      ban_type: 'nuke',
+    };
+
+    const response = await fetch(`/api/v1/users/${userFound.username}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+
+    const responseBody = await response.json();
+
+    if (response.status === 200) {
+      userFoundMutate(responseBody);
+      return;
+    }
+
+    setGlobalErrorMessage(`${responseBody.message} ${responseBody.action}`);
+    return;
+  }
+
+  function OptionsMenu() {
+    return (
+      <Box sx={{ position: 'relative' }}>
+        <Box sx={{ position: 'absolute', right: 0 }}>
+          <ActionMenu>
+            <ActionMenu.Anchor>
+              <IconButton size="small" icon={KebabHorizontalIcon} aria-label="Editar usuário" />
+            </ActionMenu.Anchor>
+
+            <ActionMenu.Overlay>
+              <ActionList>
+                {!userFound?.features?.includes('nuked') && (
+                  <ActionList.Item variant="danger" onClick={handleClickNuke}>
+                    <ActionList.LeadingVisual>
+                      <TrashIcon />
+                    </ActionList.LeadingVisual>
+                    Nuke
+                  </ActionList.Item>
+                )}
+              </ActionList>
+            </ActionMenu.Overlay>
+          </ActionMenu>
+        </Box>
+      </Box>
+    );
+  }
+
+  function UserFeatures() {
+    if (!userFound?.features?.length) return null;
+
+    return (
+      <LabelGroup sx={{ display: 'flex', ml: 2 }}>
+        {userFound.features.includes('nuked') && <Label variant="danger">nuked</Label>}
+      </LabelGroup>
+    );
+  }
 
   return (
     <>
-      <DefaultLayout metadata={{ title: `${username}` }}>
+      <DefaultLayout metadata={{ title: `${userFound.username}` }}>
+        {globalErrorMessage && (
+          <Flash variant="danger" sx={{ width: '100%', mb: 4 }}>
+            {globalErrorMessage}
+          </Flash>
+        )}
+
+        <Box sx={{ width: '100%', display: 'flex', alignItems: 'flex-start' }}>
+          <Pagehead as="h1" sx={{ width: '100%', mt: 0, pt: 0, pb: 3, display: 'flex', alignItems: 'center' }}>
+            {userFound.username} <UserFeatures />
+          </Pagehead>
+          {user?.features?.includes('ban:user') && OptionsMenu()}
+        </Box>
+
         <ContentList
           contentList={contentListFound}
           pagination={pagination}
-          paginationBasePath={`/${username}/pagina`}
-          revalidatePath={`/api/v1/contents/${username}?strategy=new`}
+          paginationBasePath={`/${userFound.username}/pagina`}
+          revalidatePath={`/api/v1/contents/${userFound.username}?strategy=new`}
           emptyStateProps={{
             isLoading: isLoading,
             title: 'Nenhum conteúdo encontrado',
-            description: `${isAuthenticatedUser ? 'Você' : username} ainda não fez nenhuma publicação.`,
+            description: `${isAuthenticatedUser ? 'Você' : userFound.username} ainda não fez nenhuma publicação.`,
             icon: FaUser,
             action: isAuthenticatedUser && {
               text: 'Publicar conteúdo',
@@ -77,6 +181,7 @@ export async function getStaticProps(context) {
       per_page: context.params.per_page,
     });
   } catch (error) {
+    // `content` model will throw a `NotFoundError` if the user is not found.
     if (error instanceof NotFoundError) {
       return {
         notFound: true,
@@ -89,13 +194,16 @@ export async function getStaticProps(context) {
 
   const contentListFound = results.rows;
 
-  const secureContentValues = authorization.filterOutput(userTryingToGet, 'read:content:list', contentListFound);
+  const secureContentListFound = authorization.filterOutput(userTryingToGet, 'read:content:list', contentListFound);
+
+  const userFound = await user.findOneByUsername(context.params.username);
+  const secureUserFound = authorization.filterOutput(userTryingToGet, 'read:user', userFound);
 
   return {
     props: {
-      contentListFound: JSON.parse(JSON.stringify(secureContentValues)),
+      contentListFound: JSON.parse(JSON.stringify(secureContentListFound)),
       pagination: results.pagination,
-      username: context.params.username,
+      userFound: JSON.parse(JSON.stringify(secureUserFound)),
     },
 
     revalidate: 1,
