cd096debd2007805b4311591eaf349031a2031ac
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2022-10-28 22:50:56 +0000

    fix(analytics): remove i18n from next.config.js

--- pages/_document.public.js
+++ pages/_document.public.js
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -25,5 +25,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
