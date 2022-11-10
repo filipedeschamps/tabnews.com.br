bd2fc825172be2ced76b2c5b31bae215c4100392
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2022-11-10 20:44:03 +0000

    feat(rate-limit): added 4s upstash service timeout

--- middleware.public.js
+++ middleware.public.js
@@ -2,14 +2,6 @@ import { NextResponse } from 'next/server';
 import rateLimit from 'infra/rate-limit.js';
 import snakeize from 'snakeize';
 
-// TODO: Next.js still didn't fix this bug:
-// https://github.com/vercel/next.js/issues/39262
-// But this bug doesn't affect Preview and Production environments.
-// So it's time enable the middleware on all routes, but then
-// we need to skip these two tests for now as a collateral effect:
-// https://github.com/filipedeschamps/tabnews.com.br/blob/35bd984db122f30ae3ed7a3b5d7baf830669a345/tests/integration/api/v1/contents/post.test.js#L268
-// https://github.com/filipedeschamps/tabnews.com.br/blob/35bd984db122f30ae3ed7a3b5d7baf830669a345/tests/integration/api/v1/contents/%5Busername%5D/%5Bslug%5D/patch.test.js#L500
-
 export const config = {
   matcher: ['/api/:path*'],
 };
