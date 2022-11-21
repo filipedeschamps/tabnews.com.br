07a258d6ff8461050035cb823cd35c0358282d98
Filipe Deschamps <filiped@gmail.com>
2022-11-21 11:07:27 -0800

    fix(contents): disable contents and API (temporary)

--- middleware.public.js
+++ middleware.public.js
@@ -8,6 +8,8 @@ export const config = {
 
 export async function middleware(request) {
   const url = request.nextUrl;
+  url.pathname = '/api/v1/_responses/rate-limit-reached';
+  return NextResponse.rewrite(url);
 
   try {
     const rateLimitResult = await rateLimit.check(request);
