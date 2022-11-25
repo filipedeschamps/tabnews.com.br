a85f9c4d2c3f9a0e11b154c585aaf9f7927019b6
ezequiaslopesdasilva <wyskzito@gmail.com>
2022-11-25 17:59:40 +0000

    feat(modal): create modal component with `@primer/react` Dialog
    changes #923

---
+++ pages/interface/components/Modal/index.js
@@ -0,0 +1,19 @@
+import { Box, Dialog, Button, Text } from '@primer/react';
+
+export default function Modal({ title, isOpen, onClose, description, customButtons }) {
+  const modalButtons = [customButtons ?? <Button sx={{ mr: 1 }} onClick={onClose} children="Fechar" />].flat(Infinity);
+
+  return (
+    <Dialog isOpen={isOpen} onDismiss={onClose} aria-labelledby="header-id">
+      <Dialog.Header id="header-id" pr={5}>
+        {title}
+      </Dialog.Header>
+      <Box p={3}>
+        <Text fontFamily="sans-serif">{description}</Text>
+        <Box display="flex" mt={3} justifyContent="flex-end">
+          {modalButtons}
+        </Box>
+      </Box>
+    </Dialog>
+  );
+}
