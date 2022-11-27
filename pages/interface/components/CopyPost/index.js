f7b4aa286320120fec319dd307aab23be75e2d00
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-27 18:53:08 -0300

    feat(copypost): atualizando o texto no botao de copiar

--- pages/interface/components/CopyPost/index.js
+++ pages/interface/components/CopyPost/index.js
@@ -1,19 +1,22 @@
+import { useState } from 'react';
 import { Box, Button } from '@primer/react';
 import { FiCopy } from 'react-icons/fi';
 
 export default function CopyPost({ objectContent }) {
+  const [msgCopy, setMsgCopy] = useState('Copiar');
   const msgToCopy = `${objectContent.title}. Essa notícia saiu no TabNews (e você ganha TabCoins comentando lá): https://www.tabnews.com.br/${objectContent.owner_username}/${objectContent.slug}`;
 
   function copyPost() {
     navigator.clipboard.writeText(msgToCopy);
-    alert('O conteúdo foi copiado para a área de transferência');
+    setMsgCopy('Post copiado com sucesso');
+    setTimeout(() => setMsgCopy('Copiar'), 4000);
   }
 
   return (
     <Box>
       {objectContent.title !== null && (
         <Button variant="invisible" leadingIcon={FiCopy} size="small" onClick={copyPost}>
-          Copiar
+          {msgCopy}
         </Button>
       )}
     </Box>
