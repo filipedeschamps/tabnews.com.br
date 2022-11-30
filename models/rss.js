5cd9b3ee501c0d1493e1c8afef931f64c3d457a9
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2022-11-30 20:22:18 +0000

    refactor(markdown viewer): prepared for future viewer package change

--- models/rss.js
+++ models/rss.js
@@ -1,5 +1,5 @@
 import { renderToStaticMarkup } from 'react-dom/server';
-import { Viewer } from '@bytemd/react';
+import { Viewer } from 'pages/interface';
 import removeMarkdown from 'models/remove-markdown';
 
 import { Feed } from 'feed';
@@ -13,7 +13,6 @@ import gemojiPlugin from '@bytemd/plugin-gemoji';
 
 function generateRss2(contentList) {
   const webserverHost = webserver.getHost();
-  const bytemdPluginList = [gfmPlugin(), highlightSsrPlugin(), mermaidPlugin(), breaksPlugin(), gemojiPlugin()];
 
   // TODO: make this property flexible in the future to
   // support things like: `/[username]/rss`
@@ -41,10 +40,7 @@ function generateRss2(contentList) {
       id: contentUrl,
       link: contentUrl,
       description: removeMarkdown(contentObject.body).replace(/\s+/g, ' ').substring(0, 190) + '...',
-      content: renderToStaticMarkup(<Viewer value={contentObject.body} plugins={bytemdPluginList} />).replace(
-        /[\r\n]/gm,
-        ''
-      ),
+      content: renderToStaticMarkup(<Viewer value={contentObject.body} />).replace(/[\r\n]/gm, ''),
       author: [
         {
           name: contentObject.owner_username,
