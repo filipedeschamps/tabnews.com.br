c40070ab89a4bb54f0d71804210155ad06f8c1d1
Filipe Deschamps <filiped@gmail.com>
2022-12-01 21:13:20 -0800

    feat(middleware): refuse connections from other hosts on production

--- middleware.public.js
+++ middleware.public.js
@@ -1,16 +1,36 @@
 import { NextResponse } from 'next/server';
+import logger from 'infra/logger.js';
 import rateLimit from 'infra/rate-limit.js';
 import snakeize from 'snakeize';
+import { UnauthorizedError } from '/errors/index.js';
+import ip from 'models/ip.js';
 
 export const config = {
   matcher: ['/api/:path*'],
 };
 
 export async function middleware(request) {
-  console.log({
-    host_get: request.headers.get('host'),
-    VERCEL_URL: process.env.VERCEL_URL,
-  });
+  if (process.env.VERCEL_ENV === 'production' && request.headers.get('host') != 'www.tabnews.com.br') {
+    const publicErrorObject = new UnauthorizedError({
+      message: 'Host não autorizado. Por favor, acesse https://www.tabnews.com.br.',
+      action: 'Não repita esta requisição.',
+    });
+
+    const privateErrorObject = {
+      ...publicErrorObject,
+      context: {
+        clientIp: ip.extractFromRequest(request),
+      },
+    };
+    logger.info(snakeize(privateErrorObject));
+
+    return new NextResponse(JSON.stringify(publicErrorObject), {
+      status: 401,
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  }
 
   const url = request.nextUrl;
 
