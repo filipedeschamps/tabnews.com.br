977f92fb85392ef825e00209702b84fbc20c2a81
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-17 23:04:52 -0300

    refactor(register): button to view the password of the page following the design pattern

--- pages/cadastro/index.public.js
+++ pages/cadastro/index.public.js
@@ -188,12 +188,14 @@ function SignUpForm() {
               block={true}
               aria-label="Sua senha"
             />
-            <button
+            <Button
+              size="small"
               type="button"
               onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-              style={{ border: 'none', background: 'none', position: 'absolute', right: 10, top: 10 }}>
+              aria-label="Visualizar senha"
+              sx={{ border: 'none', background: 'none', position: 'absolute', right: 1, top: 2 }}>
               {isPasswordVisible ? <EyeIcon size={20} /> : <EyeClosedIcon size={20} />}
-            </button>
+            </Button>
           </span>
           {capsLockWarningMessage && (
             <FormControl.Validation variant="warning">{capsLockWarningMessage}</FormControl.Validation>
