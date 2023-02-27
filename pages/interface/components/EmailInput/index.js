4f89f3e1b54425b99ba2b1b84bc75cd36312b0e7
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-27 20:06:30 -0300

    feat(component): create the email input component

---
+++ pages/interface/components/EmailInput/index.js
@@ -0,0 +1,51 @@
+import { Box, Button, FormControl, TextInput } from '@primer/react';
+
+export function EmailInput(props) {
+  const { inputRef, passwordRef, id, name, label, errorObject, setErrorObject } = props;
+
+  function clearErrors() {
+    setErrorObject(undefined);
+  }
+
+  return (
+    <FormControl id={id}>
+      <FormControl.Label>{label}</FormControl.Label>
+      <TextInput
+        ref={inputRef}
+        onChange={clearErrors}
+        name={name}
+        size="large"
+        autoCorrect="off"
+        autoCapitalize="off"
+        spellCheck={false}
+        block={true}
+        aria-label="Seu email"
+        sx={{ paddingY: '15px' }}
+      />
+      {errorObject?.key === 'email' && (
+        <FormControl.Validation variant="error">{errorObject.message}</FormControl.Validation>
+      )}
+      {errorObject?.key === 'email' && errorObject?.type === 'typo' && (
+        <FormControl.Validation variant="error">
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box>Você quis dizer:</Box>
+            <Box>
+              <Button
+                variant="invisible"
+                size="small"
+                sx={{ p: 1 }}
+                onClick={(event) => {
+                  event.preventDefault();
+                  clearErrors();
+                  inputRef.current.value = errorObject.suggestion;
+                  passwordRef.current.focus();
+                }}>
+                {errorObject.suggestion.split('@')[0]}@<u>{errorObject.suggestion.split('@')[1]}</u>
+              </Button>
+            </Box>
+          </Box>
+        </FormControl.Validation>
+      )}
+    </FormControl>
+  );
+}
