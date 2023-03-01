100dc17b1201e795e7f845143bb74b3bc1e16a82
Ryann Ferreira <ryannnkl@gmail.com>
2023-03-01 22:40:13 +0000

    style: change Dialog to ActionMenu

--- pages/interface/components/Header/index.js
+++ pages/interface/components/Header/index.js
@@ -1,66 +1,17 @@
-import {
-  Header,
-  Box,
-  ActionMenu,
-  ActionList,
-  IconButton,
-  Truncate,
-  Text,
-  Tooltip,
-  NavList,
-  Dialog,
-} from '@primer/react';
-import { PersonFillIcon, HomeIcon, SquareFillIcon } from '@primer/octicons-react';
-import { CgTab, CgMenu } from 'react-icons/cg';
+import { Header, Box, ActionMenu, ActionList, IconButton, Truncate, Text, Tooltip } from '@primer/react';
+import { PersonFillIcon, HomeIcon, SquareFillIcon, ThreeBarsIcon } from '@primer/octicons-react';
+import { CgTab } from 'react-icons/cg';
 import { HeaderLink, Link, useUser } from 'pages/interface';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 export default function HeaderComponent() {
   const { user, isLoading, logout } = useUser();
-  const [showMenuMobile, setShowMenuMobile] = useState(false);
 
-  const { pathname, asPath } = useRouter();
+  const { pathname } = useRouter();
 
   const activeLinkStyle = {
     textDecoration: 'underline',
     textUnderlineOffset: 6,
-  };
-
-  function NavItem({ href, children }) {
-    const isCurrent = typeof href === 'string' ? asPath === href : pathname === href.pathname;
-    return (
-      <Link href={href} passHref>
-        <NavList.Item sx={{ fontSize: 18 }} aria-current={isCurrent ? 'page' : false}>
-          {children}
-        </NavList.Item>
-      </Link>
-    );
-  }
-
-  const menuMobile = () => {
-    return (
-      <>
-        <Header.Item sx={{ display: ['flex', 'none'] }}>
-          <CgMenu size={32} onClick={() => setShowMenuMobile(true)} />
-        </Header.Item>
-        <Dialog
-          isOpen={showMenuMobile}
-          onDismiss={() => setShowMenuMobile(false)}
-          aria-labelledby="menu"
-          sx={{ color: 'dark' }}>
-          <Dialog.Header sx={{ backgroundColor: '#24292F' }}>
-            <CgTab />
-          </Dialog.Header>
-          <NavList>
-            <NavItem href="/" aria-current="page">
-              Relevantes
-            </NavItem>
-            <NavItem href="/recentes">Recentes</NavItem>
-          </NavList>
-        </Dialog>
-      </>
-    );
   };
 
   return (
@@ -69,7 +20,26 @@ export default function HeaderComponent() {
       sx={{
         px: [2, null, null, 3],
       }}>
-      {menuMobile()}
+      <Header.Item sx={{ display: ['flex', 'none'] }}>
+        <ActionMenu>
+          <ActionMenu.Anchor>
+            <IconButton icon={ThreeBarsIcon} size="small" aria-label="Abrir menu" />
+          </ActionMenu.Anchor>
+          <ActionMenu.Overlay>
+            <ActionList>
+              <ActionList.Item sx={{ fontSize: 16 }}>TabNews</ActionList.Item>
+              <ActionList.Divider />
+              <ActionList.Item as={Link} href="/">
+                Relevantes
+              </ActionList.Item>
+              <ActionList.Item as={Link} href="/recentes">
+                Recentes
+              </ActionList.Item>
+            </ActionList>
+          </ActionMenu.Overlay>
+        </ActionMenu>
+      </Header.Item>
+
       <Header.Item>
         <HeaderLink href="/" aria-label="Voltar para a página inicial">
           <CgTab size={32} />
