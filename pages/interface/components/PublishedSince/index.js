04b6baadb223ae7992917b0ab948794c98174301
aprendendofelipe <77860630+aprendendofelipe@users.noreply.github.com>
2023-03-06 00:57:54 -0300

    fix(date tooltip): fix client time zone

--- pages/interface/components/PublishedSince/index.js
+++ pages/interface/components/PublishedSince/index.js
@@ -1,6 +1,7 @@
 import { formatDistanceToNowStrict, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { Tooltip } from '@primer/react';
+import { useEffect, useState } from 'react';
 
 function formatPublishedSince(date) {
   const publishedSince = formatDistanceToNowStrict(new Date(date), {
@@ -10,13 +11,21 @@ function formatPublishedSince(date) {
   return `${publishedSince} atrás`;
 }
 
-function formatTooltipLabel(date) {
-  return format(new Date(date), "EEEE, d 'de' MMMM 'de' yyyy 'às' HH:mm", { locale: ptBR });
+function formatTooltipLabel(date, gmt = false) {
+  const displayFormat = gmt ? "EEEE, d 'de' MMMM 'de' yyyy 'às' HH:mm z" : "EEEE, d 'de' MMMM 'de' yyyy 'às' HH:mm";
+
+  return format(new Date(date), displayFormat, { locale: ptBR });
 }
 
 export default function PublishedSince({ date, ...props }) {
+  const [tooltipLabel, setTooltipLabel] = useState(formatTooltipLabel(date, true));
+
+  useEffect(() => {
+    setTooltipLabel(formatTooltipLabel(date));
+  }, [date]);
+
   return (
-    <Tooltip sx={{ position: 'absolute', ml: 1 }} aria-label={formatTooltipLabel(date)} {...props}>
+    <Tooltip sx={{ position: 'absolute', ml: 1 }} aria-label={tooltipLabel} suppressHydrationWarning {...props}>
       <span style={{ whiteSpace: 'nowrap' }} suppressHydrationWarning>
         {formatPublishedSince(date)}
       </span>
