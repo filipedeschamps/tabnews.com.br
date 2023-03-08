686bf42cefb019595b0377c9f5609bdc5e1b9db5
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2023-03-07 22:08:20 -0300

    fix(password input): added padding to `trailingAction`

--- pages/interface/components/PasswordInput/index.js
+++ pages/interface/components/PasswordInput/index.js
@@ -49,7 +49,7 @@ export default function PasswordInput({ inputRef, id, name, label, errorObject, 
             aria-label={isPasswordVisible ? 'Ocultar a senha' : 'Visualizar a senha'}
             onClick={handlePasswordVisible}
             icon={isPasswordVisible ? EyeClosedIcon : EyeIcon}
-            sx={{ color: 'fg.subtle', padding: '0' }}
+            sx={{ color: 'fg.subtle' }}
           />
         }
         ref={inputRef}
