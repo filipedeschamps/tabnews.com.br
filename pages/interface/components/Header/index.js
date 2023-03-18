b44ed47582e52f5bc2618fde84912e0bc83d6931
Ryann Ferreira <ryannnkl@gmail.com>
2023-03-18 05:03:10 +0000

    style: update link to NavItem

--- pages/interface/components/Header/index.js
+++ pages/interface/components/Header/index.js
@@ -1,11 +1,14 @@
-import { Header, Box, ActionMenu, ActionList, IconButton, Truncate, Text, Tooltip } from '@primer/react';
+import { Header, Box, ActionMenu, ActionList, IconButton, Truncate, Text, Tooltip, Button } from '@primer/react';
 import { PersonFillIcon, HomeIcon, SquareFillIcon, ThreeBarsIcon } from '@primer/octicons-react';
 import { CgTab } from 'react-icons/cg';
-import { HeaderLink, Link, useUser } from 'pages/interface';
+import { HeaderLink, Link, NavItem, useUser } from 'pages/interface';
 import { useRouter } from 'next/router';
+import { createRef, useState } from 'react';
 
 export default function HeaderComponent() {
   const { user, isLoading, logout } = useUser();
+  const [open, setOpen] = useState(false);
+  const anchorRef = createRef();
 
   const { pathname } = useRouter();
 
@@ -19,20 +22,26 @@ export default function HeaderComponent() {
       id="header"
       sx={{
         px: [2, null, null, 3],
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}>
-      <Header.Item sx={{ display: ['flex', 'none'] }}>
-        <ActionMenu>
-          <ActionMenu.Anchor>
-            <IconButton icon={ThreeBarsIcon} size="small" aria-label="Open column options" />
-          </ActionMenu.Anchor>
+      <Header.Item sx={{ display: ['flex', 'none'] }} full>
+        <Button
+          variant="invisible"
+          ref={anchorRef}
+          aria-haspopup="true"
+          aria-expanded={open}
+          style={{ background: 'transparent', border: 'none' }}
+          sx={{ color: 'fg.onEmphasis' }}
+          onClick={() => setOpen(!open)}>
+          <ThreeBarsIcon size={24} />
+        </Button>
+        <ActionMenu open={open} onOpenChange={setOpen} anchorRef={anchorRef}>
           <ActionMenu.Overlay>
             <ActionList>
-              <ActionList.Item as={Link} href="/">
-                Relevantes
-              </ActionList.Item>
-              <ActionList.Item as={Link} href="/recentes">
-                Recentes
-              </ActionList.Item>
+              <NavItem href="/">Relevantes</NavItem>
+              <NavItem href="/recentes">Recentes</NavItem>
               {user && (
                 <>
                   <ActionList.Divider />
@@ -56,9 +65,7 @@ export default function HeaderComponent() {
         </ActionMenu>
       </Header.Item>
 
-      <Header.Item sx={{ display: ['block', 'none'] }} full />
-
-      <Header.Item>
+      <Header.Item sx={{ mx: 'auto' }}>
         <HeaderLink href="/" aria-label="Voltar para a página inicial">
           <CgTab size={32} />
           <Box sx={{ ml: 2, display: ['none', 'block'] }}>TabNews</Box>
@@ -133,15 +140,9 @@ export default function HeaderComponent() {
                     <Truncate>{user.username}</Truncate>
                   </ActionList.LinkItem>
                   <ActionList.Divider />
-                  <ActionList.LinkItem as={Link} href="/publicar">
-                    Publicar novo conteúdo
-                  </ActionList.LinkItem>
-                  <ActionList.LinkItem as={Link} href={`/${user.username}`}>
-                    Meus conteúdos
-                  </ActionList.LinkItem>
-                  <ActionList.LinkItem as={Link} href="/perfil">
-                    Editar perfil
-                  </ActionList.LinkItem>
+                  <NavItem href="/publicar">Publicar novo conteúdo</NavItem>
+                  <NavItem href={`/${user.username}`}>Meus conteúdos</NavItem>
+                  <NavItem href="/perfil">Editar perfil</NavItem>
                   <ActionList.Divider />
                   <ActionList.Item variant="danger" onSelect={logout}>
                     Deslogar
