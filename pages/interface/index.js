0da8932696a016f86adbe655ad7a49be9cd3b1be
Joanderson Paiva <joandersonpaiva+github@gmail.com>
2023-05-31 20:47:12 -0300

    fix: email suggestion

--- pages/interface/index.js
+++ pages/interface/index.js
@@ -2,3 +2,4 @@ export { default as Head, DefaultHead } from './components/Head/index.js';
 export { default as useCollapse } from './hooks/useCollapse/index.js';
 export { default as useMediaQuery } from './hooks/useMediaQuery/index.js';
 export { default as useUser, UserProvider } from './hooks/useUser/index.js';
+export { default as suggestEmail } from './utils/email-suggestion';
