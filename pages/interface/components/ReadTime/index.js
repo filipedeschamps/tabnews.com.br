cfea8014fda55b05e101019185e9d1dc08e7a4e5
ValbertMartins <valbertclash@gmail.com>
2023-06-24 15:17:24 -0300

    perf: change getReadTime to inside useMemo

--- pages/interface/components/ReadTime/index.js
+++ pages/interface/components/ReadTime/index.js
@@ -1,14 +1,16 @@
 import { Text } from '@/TabNewsUI';
-
-function getReadTime(text, wpm = 260) {
-  const wordsQuantity = text.split(' ').length;
-  return Math.ceil(wordsQuantity / wpm);
-}
+import { useMemo } from 'react';
 
 export default function ReadTime({ text, ...props }) {
+  const readTimeInMinutes = useMemo(() => {
+    const wpm = 260;
+    const wordsQuantity = text.split(' ').length;
+    return Math.ceil(wordsQuantity / wpm);
+  }, [text]);
+
   return (
     <Text sx={{ fontSize: 0, color: 'fg.muted', py: '1px', height: '22px' }} {...props}>
-      {`${getReadTime(text)} ${getReadTime(text) === 1 ? 'minuto' : 'minutos'} de leitura`}
+      {`${readTimeInMinutes} ${readTimeInMinutes === 1 ? 'minuto' : 'minutos'} de leitura`}
     </Text>
   );
 }
