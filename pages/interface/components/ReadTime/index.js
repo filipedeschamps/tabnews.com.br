834e3c3b0dce62aa5d881e1672529451486ece17
ValbertMartins <valbertclash@gmail.com>
2023-06-25 14:42:02 -0300

    refactor: improve readtime component

--- pages/interface/components/ReadTime/index.js
+++ pages/interface/components/ReadTime/index.js
@@ -4,13 +4,13 @@ import { useMemo } from 'react';
 export default function ReadTime({ text, ...props }) {
   const readTimeInMinutes = useMemo(() => {
     const wpm = 260;
-    const wordsQuantity = text.split(' ').length;
-    return Math.ceil(wordsQuantity / wpm);
+    const wordsQuantity = text.split(/[^A-Za-z]+/).length;
+    return `${Math.ceil(wordsQuantity / wpm)} min de leitura`;
   }, [text]);
 
   return (
     <Text sx={{ fontSize: 0, color: 'fg.muted', py: '1px', height: '22px' }} {...props}>
-      {`${readTimeInMinutes} ${readTimeInMinutes === 1 ? 'minuto' : 'minutos'} de leitura`}
+      {readTimeInMinutes}
     </Text>
   );
 }
