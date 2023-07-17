8af7e9995edbe75986a64c99dc9e466102ff7cb6
ValbertMartins <valbertclash@gmail.com>
2023-07-17 18:54:11 -0300

    refactor: improve description migration

--- infra/migrations/1688768453152_alter-table-users-add-description.js
+++ infra/migrations/1688768453152_alter-table-users-add-description.js
@@ -1,7 +1,10 @@
 exports.up = async (pgm) => {
   await pgm.addColumns('users', {
     description: {
-      type: 'varchar(160)',
+      type: 'text',
+      check: 'length(description) <= 5000',
+      notNull: true,
+      default: '',
     },
   });
 };
