59a93d0344f4bdf1b00ea8ff7f99cd14f7738915
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2023-08-29 15:35:24 -0300

    Adiciona uma aba "Antigos"
    Funcionando exatamente como a aba "Recentes" mas em ordem cronológica inversa

---
+++ pages/antigos/index.public.js
@@ -0,0 +1,78 @@
+import { getStaticPropsRevalidate } from 'next-swr';
+
+import { Box, ContentList, Heading, Viewer, DefaultLayout } from '@/TabNewsUI';
+import authorization from 'models/authorization.js';
+import content from 'models/content.js';
+import user from 'models/user.js';
+import validator from 'models/validator.js';
+
+export default function Home({ contentListFound, pagination }) {
+  const body = `  
+
+  Aqui você vai encontrar, **notícias**, **artigos**, **tutoriais**, **indicações**, **curiosidades** e dúvidas respondidas ou que você pode responder ordenadas pelo conteúdo mais antigo, veja também o nosso [FAQ](faq)
+
+ <br>
+ 
+  `;
+
+
+  return (
+    <>
+      <DefaultLayout
+        metadata={{
+          title: 'Antigos',
+          description: 'Publicações no TabNews ordenadas pelas mais antigas.',
+        }}>
+      <Box>
+        <Viewer value={body} />
+      </Box>
+
+        <ContentList contentList={contentListFound} pagination={pagination} paginationBasePath="/antigas/pagina" />
+
+      </DefaultLayout>
+    </>
+  );
+}
+
+export const getStaticProps = getStaticPropsRevalidate(async (context) => {
+  const userTryingToGet = user.createAnonymous();
+
+  context.params = context.params ? context.params : {};
+
+  try {
+    context.params = validator(context.params, {
+      page: 'optional',
+      per_page: 'optional',
+    });
+  } catch (error) {
+    return {
+      notFound: true,
+      revalidate: 1,
+    };
+  }
+
+  const results = await content.findWithStrategy({
+    strategy: 'old',
+    where: {
+      parent_id: null,
+      status: 'published',
+    },
+    attributes: {
+      exclude: ['body'],
+    },
+    page: context.params.page,
+    per_page: context.params.per_page,
+  });
+
+  const contentListFound = results.rows;
+
+  const secureContentValues = authorization.filterOutput(userTryingToGet, 'read:content:list', contentListFound);
+
+  return {
+    props: {
+      contentListFound: JSON.parse(JSON.stringify(secureContentValues)),
+      pagination: results.pagination,
+    },
+    revalidate: 10,
+  };
+});
---
+++ pages/antigos/pagina/[page]/index.public.js
@@ -0,0 +1,64 @@
+import { getStaticPropsRevalidate } from 'next-swr';
+
+import { ContentList, DefaultLayout } from '@/TabNewsUI';
+import authorization from 'models/authorization.js';
+import content from 'models/content.js';
+import user from 'models/user.js';
+import validator from 'models/validator.js';
+
+export default function Home({ contentListFound, pagination }) {
+  return (
+    <>
+      <DefaultLayout metadata={{ title: `Página ${pagination.currentPage} · Recentes` }}>
+        <ContentList contentList={contentListFound} pagination={pagination} paginationBasePath="/recentes/pagina" />
+      </DefaultLayout>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { page: '2' } }, { params: { page: '3' } }],
+    fallback: 'blocking',
+  };
+}
+
+export const getStaticProps = getStaticPropsRevalidate(async (context) => {
+  const userTryingToGet = user.createAnonymous();
+
+  context.params = context.params ? context.params : {};
+
+  try {
+    context.params = validator(context.params, {
+      page: 'optional',
+      per_page: 'optional',
+    });
+  } catch (error) {
+    return {
+      notFound: true,
+      revalidate: 1,
+    };
+  }
+
+  const results = await content.findWithStrategy({
+    strategy: 'old',
+    where: {
+      parent_id: null,
+      status: 'published',
+    },
+    page: context.params.page,
+    per_page: context.params.per_page,
+  });
+
+  const contentListFound = results.rows;
+
+  const secureContentValues = authorization.filterOutput(userTryingToGet, 'read:content:list', contentListFound);
+
+  return {
+    props: {
+      contentListFound: JSON.parse(JSON.stringify(secureContentValues)),
+      pagination: results.pagination,
+    },
+    revalidate: 10,
+  };
+});
