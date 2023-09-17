58620ee38642a667790e6998288e9e7f5fd3fcf1
coffee-is-power <tiagodinis33@proton.me>
2023-09-17 12:33:27 +0100

    chore: add zellij to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,20 @@
 let
-  pkgs = import <nixpkgs> { 
+  unstablePkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/3c15feef7770eb5500a4b8792623e2d6f598c9c1.tar.gz";
+  }) {};
+  pkgs = import <nixpkgs> {
     config.permittedInsecurePackages = [ "nodejs-16.20.1" ];
   };
 in
 pkgs.mkShell {
   buildInputs = [
+    unstablePkgs.bun
     pkgs.nodejs_16
     pkgs.nodePackages.npm
     pkgs.nodePackages.typescript-language-server
     pkgs.docker-compose
     pkgs.docker-client
+    pkgs.zellij
   ];
 }
 
