84bb110949ba08bf7059a5b3a7b5946e82254148
Ytalo <90343384+eletroswing@users.noreply.github.com>
2023-11-03 22:20:18 +0000

    style(prettier): running prettier

--- infra/migrations/1698968536361_create-captcha-table.js
+++ infra/migrations/1698968536361_create-captcha-table.js
@@ -1,42 +1,42 @@
 /* eslint-disable camelcase */
 
 exports.up = (pgm) => {
-    pgm.createTable('captchas', {
-        id: {
-          type: 'uuid',
-          default: pgm.func('gen_random_uuid()'),
-          notNull: true,
-          primaryKey: true,
-        },
-        
-        token: {
-          type: 'varchar(60)',
-          notNull: true,
-        },
-    
-        expires_at: {
-          type: 'timestamp with time zone',
-          notNull: true,
-        },
+  pgm.createTable('captchas', {
+    id: {
+      type: 'uuid',
+      default: pgm.func('gen_random_uuid()'),
+      notNull: true,
+      primaryKey: true,
+    },
 
-        used: {
-          type: 'boolean',
-          notNull: true,
-          default: false,
-        },
-    
-        created_at: {
-          type: 'timestamp with time zone',
-          notNull: true,
-          default: pgm.func("(now() at time zone 'utc')"),
-        },
-    
-        updated_at: {
-          type: 'timestamp with time zone',
-          notNull: true,
-          default: pgm.func("(now() at time zone 'utc')"),
-        },
-      });
+    token: {
+      type: 'varchar(60)',
+      notNull: true,
+    },
+
+    expires_at: {
+      type: 'timestamp with time zone',
+      notNull: true,
+    },
+
+    used: {
+      type: 'boolean',
+      notNull: true,
+      default: false,
+    },
+
+    created_at: {
+      type: 'timestamp with time zone',
+      notNull: true,
+      default: pgm.func("(now() at time zone 'utc')"),
+    },
+
+    updated_at: {
+      type: 'timestamp with time zone',
+      notNull: true,
+      default: pgm.func("(now() at time zone 'utc')"),
+    },
+  });
 };
 
 exports.down = (pgm) => {
