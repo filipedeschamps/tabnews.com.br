ec5e22b6c99b961a90004d06a8a4e5301d79c509
Rafael Tavares <26308880+Rafatcb@users.noreply.github.com>
2023-12-01 21:53:09 -0300

    feat(user): add more info to user profile
    Add TabCoins, TabCash and account creation date in the Pagehead; add an ActionMenu so the user can
    edit their own profile when visiting it.

---
+++ pages/interface/components/TabCoinCount/index.js
@@ -0,0 +1,13 @@
+import { Box, Text, Tooltip } from '@/TabNewsUI';
+import { SquareFillIcon } from '@/TabNewsUI/icons';
+
+export default function TabCoinCount({ amount, direction, sx }) {
+  return (
+    <Tooltip aria-label="TabCoins" direction={direction ?? 's'} noDelay={true} wrap={true}>
+      <Box sx={{ display: 'flex', alignItems: 'center', ...sx }}>
+        <SquareFillIcon fill="#0969da" size={16} />
+        <Text>{amount?.toLocaleString('pt-BR')}</Text>
+      </Box>
+    </Tooltip>
+  );
+}
