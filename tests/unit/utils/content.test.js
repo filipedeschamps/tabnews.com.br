eda3980dafdfefc26ecdd12e483cb8e8940224a7
Tiago Davi <tiago.asp.net@gmail.com>
2024-01-25 11:17:06 -0300

    refactor(isContentTooShort/tests): empty content is also too short

--- tests/unit/utils/content.test.js
+++ tests/unit/utils/content.test.js
@@ -12,4 +12,10 @@ describe('.isContentTooShort', () => {
     const content = 'palhaçada que você disse para flávia beber cachaça!';
     expect(isContentTooShort(content, minimumNumWords)).toBe(false);
   });
+
+  it('Should return true when content is empty', async () => {
+    const minimumNumWords = 5;
+    const content = '';
+    expect(isContentTooShort(content, minimumNumWords)).toBe(true);
+  });
 });
--- utils/content.js
+++ utils/content.js
@@ -1,5 +1,8 @@
 export function isContentTooShort(content, minimumNumWords) {
   const regexPattern = new RegExp(`[a-záéíóúâôãõç]{${minimumNumWords},}`, 'gi');
   const matches = content.normalize('NFC').match(regexPattern);
-  return matches && matches.length < minimumNumWords;
+
+  if (!matches) return true;
+
+  return matches.length < minimumNumWords;
 }
