a34a34ff8efaf576490733a7c774a41c3e6ceac2
Tiago Davi <tiago.asp.net@gmail.com>
2024-01-25 11:04:25 -0300

    refactor(isContentTooShort/tests): numCharacthers to minimumNumWords

--- tests/unit/utils/content.test.js
+++ tests/unit/utils/content.test.js
@@ -1,15 +1,15 @@
 import { isContentTooShort } from 'utils/content';
 
 describe('.isContentTooShort', () => {
-  it('Should return true when content is shorter than numCharacters', async () => {
-    const numCharacters = 5;
+  it('Should return true when content is shorter than minimumNumWords', async () => {
+    const minimumNumWords = 5;
     const content = 'já concordei ctg.';
-    expect(isContentTooShort(content, numCharacters)).toBe(true);
+    expect(isContentTooShort(content, minimumNumWords)).toBe(true);
   });
 
-  it('Should return false when content is NOT shorter than numCharacters', async () => {
-    const numCharacters = 5;
+  it('Should return false when content is NOT shorter than minimumNumWords', async () => {
+    const minimumNumWords = 5;
     const content = 'palhaçada que você disse para flávia beber cachaça!';
-    expect(isContentTooShort(content, numCharacters)).toBe(false);
+    expect(isContentTooShort(content, minimumNumWords)).toBe(false);
   });
 });
