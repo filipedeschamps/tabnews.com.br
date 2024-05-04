405aaf5bb1e64a2c6fecd88aaec33da23255a86e
renatão <paulo05renato@gmail.com>
2024-05-04 11:43:08 -0300

    feat(test e2e): added dependency playwright and your config file

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+import { config } from 'dotenv';
+
+config({ path: './.env.test' });
+
+const PORT = process.env.PORT || 3000;
+const baseURL = `http://localhost:${PORT}`;
+
+export default defineConfig({
+  testDir: './tests/e2e/',
+  testMatch: '**/*.spec.js',
+  reporter: [['html', { open: 'never' }]],
+  workers: 1,
+  use: {
+    baseURL,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
