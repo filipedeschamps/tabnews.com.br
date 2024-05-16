9a48d76165ffc32fe39c2e9a9d93e4219d71bf82
Matheus Calixto <mth.m.calixto@gmail.com>
2024-05-16 12:30:20 -0300

    feat(searchbox): backend updates
    - remove routers /trends /suggestions
    - remove secureOutputValues
    - refactor search.js/validator.js

---
+++ infra/migrations/1715808549919_add-search-box.js
@@ -0,0 +1,26 @@
+exports.up = async (pgm) => {
+  await pgm.db.query(`
+    CREATE INDEX idx_search ON contents USING GIN(to_tsvector('portuguese', title));
+  `);
+
+  await pgm.db.query(`
+    ALTER TABLE contents 
+    ADD COLUMN ts tsvector 
+    GENERATED ALWAYS AS (
+      setweight(to_tsvector('portuguese', coalesce(title, '')), 'A') ||
+      setweight(to_tsvector('portuguese', coalesce(body, '')), 'B')
+    ) STORED;
+  `);
+
+  await pgm.db.query(`
+    CREATE INDEX idx_contents_ts ON contents USING GIN(ts);
+  `);
+};
+
+exports.down = async (pgm) => {
+  await pgm.dropColumn('contents', 'ts', { ifExists: true });
+
+  await pgm.dropIndex('contents', 'idx_search', { ifExists: true });
+
+  await pgm.dropIndex('contents', 'idx_contents_ts', { ifExists: true });
+};
