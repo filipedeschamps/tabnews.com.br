262e911761f4d3297394c06dbc45b6e69a17467a
renatão <paulo05renato@gmail.com>
2024-05-27 09:52:19 -0300

    feat(test e2e): change values `include`

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -8,7 +8,7 @@ config();
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    include: ['**/integration/**', '**/unit/**'],
+    include: ['./tests/integration/**/*.test.js', './tests/unit/**/*.test.js'],
     environmentMatchGlobs: [['**/interface/**/*', 'jsdom']],
     globals: true,
     fileParallelism: false,
