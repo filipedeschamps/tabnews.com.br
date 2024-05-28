757f60886801ba5eaa121c8cdce7a920ee26b907
renatão <paulo05renato@gmail.com>
2024-05-28 07:58:35 -0300

    feat(test e2e): update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -8,13 +8,9 @@ const PORT = process.env.PORT || 3000;
 const baseURL = `http://localhost:${PORT}`;
 
 export default defineConfig({
-  testDir: 'tests/e2e',
+  testDir: './tests/e2e/',
   testMatch: '**/*.spec.js',
   workers: process.env.CI ? 1 : undefined,
-  timeout: 30 * 1000,
-  expect: {
-    timeout: 5000,
-  },
   use: {
     baseURL,
   },
