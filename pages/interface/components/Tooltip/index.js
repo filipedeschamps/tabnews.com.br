bd298ff35dc7a7b4ab9c0c9950ac2d1ef6ea8156
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2024-06-25 13:28:46 -0300

    fix(tooltip): adjust gap between Tololtip and content to `2px`

--- pages/interface/components/Tooltip/index.js
+++ pages/interface/components/Tooltip/index.js
@@ -1,15 +1,46 @@
 import { Box, PrimerTooltip } from '@/TabNewsUI';
 
-export default function Tooltip({ children, ...props }) {
-  const { sx: { position, ...sxRest } = {} } = props;
+const gap = '2px';
+
+const replaceMargin = {
+  '&.tooltipped-s, &.tooltipped-se, &.tooltipped-sw': {
+    '&::after': {
+      marginTop: gap,
+    },
+  },
+  '&.tooltipped-e, &.tooltipped-se, &.tooltipped-ne': {
+    '&::after': {
+      marginLeft: gap,
+    },
+  },
+  '&.tooltipped-n, &.tooltipped-ne, &.tooltipped-nw': {
+    '&::after': {
+      marginBottom: gap,
+    },
+  },
+  '&.tooltipped-w, &.tooltipped-sw, &.tooltipped-nw': {
+    '&::after': {
+      marginRight: gap,
+    },
+  },
+};
+
+export default function Tooltip({ children, sx, ...props }) {
+  const { position, ...sxRest } = sx || {};
 
   if (position === 'absolute')
     return (
       <Box sx={{ display: 'inline-flex' }}>
         <Box sx={{ ...sxRest, visibility: 'hidden' }}>{children}</Box>
-        <PrimerTooltip {...props}>{children}</PrimerTooltip>
+        <PrimerTooltip sx={{ ...replaceMargin, ...sx }} {...props}>
+          {children}
+        </PrimerTooltip>
       </Box>
     );
 
-  return <PrimerTooltip {...props}>{children}</PrimerTooltip>;
+  return (
+    <PrimerTooltip sx={{ ...replaceMargin, ...sx }} {...props}>
+      {children}
+    </PrimerTooltip>
+  );
 }
