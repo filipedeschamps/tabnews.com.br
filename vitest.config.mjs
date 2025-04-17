6c238ae2e90af1e653b88474be2db7592896f41b
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-04-17 15:49:08 -0300

    chore(Vitest): update to v3

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { config } from 'dotenv';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import { defaultExclude, defineConfig } from 'vitest/config';
 
 config();
 
@@ -9,13 +9,29 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     server: { deps: { inline: ['@tabnews/ui'] } },
-    environmentMatchGlobs: [['**/interface/**/*', 'jsdom']],
     globals: true,
     fileParallelism: false,
     isolate: false,
     testTimeout: 60_000,
     hookTimeout: 30_000,
     setupFiles: ['tests/setup.js'],
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'node',
+          exclude: ['**/interface/**', ...defaultExclude],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'jsdom',
+          environment: 'jsdom',
+          include: ['**/interface/**/*.{test,spec}.{js,ts}'],
+        },
+      },
+    ],
   },
   esbuild: {
     loader: 'jsx',
