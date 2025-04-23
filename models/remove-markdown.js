beb9889d1e30b8468de9e1c96e6cc26f37a266f6
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-04-23 16:11:34 -0300

    fix(remove-markdown): ensure 'trim' option defaults to `true`

--- models/remove-markdown.js
+++ models/remove-markdown.js
@@ -5,6 +5,7 @@ import logger from 'infra/logger';
 export default function customRemoveMarkdown(md, options = {}) {
   options.oneLine = Object.hasOwn(options, 'oneLine') ? options.oneLine : true;
   options.throwError = Object.hasOwn(options, 'logError') ? options.throwError : true;
+  options.trim = Object.hasOwn(options, 'trim') ? options.trim : true;
 
   let output = md || '';
 
