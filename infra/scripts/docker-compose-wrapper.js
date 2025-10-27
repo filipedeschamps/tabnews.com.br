7399fc226acb010f747351a086076e78b56314f3
Marcone Boff <boffmarcone@gmail.com>
2025-10-27 14:22:29 -0300

    feat: add docker-compose wrapper using `cross-env`
    Installed cross-env as a dev dependency, created docker-compose-wrapper.js to control docker:compose
    script, and updated package.json scripts to use the wrapper
    
    re #1961

---
+++ infra/scripts/docker-compose-wrapper.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+import { spawnSync } from 'child_process';
+
+const isCI = process.env.CI === 'true';
+const isTest = process.env.NODE_ENV === 'test';
+
+const composeFile = isCI || isTest ? 'infra/docker-compose.ci.yml' : 'infra/docker-compose.development.yml';
+
+const args = ['compose', '--env-file', '.env', '-f', composeFile, ...process.argv.slice(2)];
+
+console.log(`🐳 Using: ${composeFile.split('/').pop()}\n`);
+
+const result = spawnSync('docker', args, { stdio: 'inherit' });
+
+process.exit(result.status);
