61eb3d198b5fa1b49703aba2fa41f195601c66dc
Rafael Tavares <26308880+Rafatcb@users.noreply.github.com>
2026-06-06 20:16:39 -0300

    chore(deps): upgrade `satori` and `feed` major versions

--- tests/integration/api/v1/contents/rss/get.test.js
+++ tests/integration/api/v1/contents/rss/get.test.js
@@ -140,18 +140,20 @@ describe('GET /recentes/rss', () => {
         <item>
             <title><![CDATA[Conteúdo #2 (mais novo)]]></title>
             <link>${orchestrator.webserverUrl}/${secondRootContent.owner_username}/${secondRootContent.slug}</link>
-            <guid>${orchestrator.webserverUrl}/${secondRootContent.owner_username}/${secondRootContent.slug}</guid>
+            <guid isPermaLink="false">${orchestrator.webserverUrl}/${secondRootContent.owner_username}/${secondRootContent.slug}</guid>
             <pubDate>${new Date(secondRootContent.published_at).toUTCString()}</pubDate>
             <description><![CDATA[Este é um corpo bastante longo, vamos ver como que a propriedade description irá reagir, pois por padrão ela deverá cortar após um número X de caracteres. Não vou tomar nota aqui da quant...]]></description>
             <content:encoded><![CDATA[<div class="markdown-body"><p>Este é um corpo bastante longo, vamos ver como que a propriedade description irá reagir, pois por padrão ela deverá cortar após um número X de caracteres. Não vou tomar nota aqui da quantidade exata de caracteres, pois isso pode mudar ao longo do tempo.</p></div>]]></content:encoded>
+            <author>${secondRootContent.owner_username}</author>
         </item>
         <item>
             <title><![CDATA[Conteúdo #1 (mais antigo)]]></title>
             <link>${orchestrator.webserverUrl}/${firstRootContent.owner_username}/${firstRootContent.slug}</link>
-            <guid>${orchestrator.webserverUrl}/${firstRootContent.owner_username}/${firstRootContent.slug}</guid>
+            <guid isPermaLink="false">${orchestrator.webserverUrl}/${firstRootContent.owner_username}/${firstRootContent.slug}</guid>
             <pubDate>${new Date(firstRootContent.published_at).toUTCString()}</pubDate>
             <description><![CDATA[Corpo com HTML É importante lidar corretamente com o HTML, incluindo estilos especiais do GFM.]]></description>
             <content:encoded><![CDATA[<div class="markdown-body"><h1 id="${defaultUser.username.toLowerCase()}-content-corpo-com-html">Corpo com HTML</h1><p>É <strong>importante</strong> lidar corretamente com o <code>HTML</code>, incluindo estilos <del>especiais</del> do <code>GFM</code>.</p></div>]]></content:encoded>
+            <author>${firstRootContent.owner_username}</author>
         </item>
     </channel>
 </rss>`);
